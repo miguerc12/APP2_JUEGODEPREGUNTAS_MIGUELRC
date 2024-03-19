04675ca5f9c306bd68929c3541c9c9f50bbccee5
miguerc12 <cobmigue329@gmail.com>
2024-03-19 12:51:03 -0600

    agregue la interfaz de la app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "APP2_JUEGOPREGUNTASFINAL_MIGUELRC"
+include(":app")
+ 
